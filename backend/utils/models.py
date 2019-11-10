0db3224cbf919b224e86cca3af772da2e39b074c
Luis Fagundes <lhfagundes@gmail.com>
2019-11-10 13:35:10 -0300

    campos ordenados em ordem alfabética

--- backend/utils/models.py
+++ backend/utils/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class ModeloComNome(models.Model):
     class Meta:
         abstract = True
+        ordering = ('nome',)
 
     def __str__(self):
         return self.nome
