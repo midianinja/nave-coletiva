9d41b1b637bcbc5a05ded30a53fea310de49a5e9
Luis Fagundes <lhfagundes@gmail.com>
2019-11-13 00:22:59 -0300

    filtro por capacidade do espaco

--- backend/nave/admin.py
+++ backend/nave/admin.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
+from admin_numeric_filter.admin import RangeNumericFilter
 
 from nave.models import Andar, Espaco
 
+@admin.register(Espaco)
 class EspacoAdmin(admin.ModelAdmin):
     list_display = ['nome', 'andar', 'capacidade']
     search_fields = ['nome']
-    list_filter = ['andar', 'capacidade']
-
+    list_filter = [
+        'andar',
+        ('capacidade', RangeNumericFilter),
+        ]
 
 admin.site.register(Andar)
-admin.site.register(Espaco, EspacoAdmin)
