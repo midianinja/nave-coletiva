0b9d37b2f1a8e7245fb0a09d3ce1134b5396d60a
Luis Fagundes <lhfagundes@gmail.com>
2019-11-12 23:29:10 -0300

    wip

--- backend/nave/admin.py
+++ backend/nave/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 
 from nave.models import Andar, Espaco
 
+class EspacoAdmin(admin.ModelAdmin):
+    list_display = ['nome', 'andar', 'capacidade']
+    search_fields = ['nome']
+    list_filter = ['andar', 'capacidade']
+
+
 admin.site.register(Andar)
-admin.site.register(Espaco)
+admin.site.register(Espaco, EspacoAdmin)
