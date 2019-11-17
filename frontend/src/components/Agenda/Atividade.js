660aa41aeedafe76d6001428e1b5d1cfdef5c06d
Isaque Costa <isaquebc@gmail.com>
2019-11-17 17:57:50 -0300

    [master]

--- frontend/src/components/Agenda/Atividade.js
+++ frontend/src/components/Agenda/Atividade.js
@@ -2,13 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const AtividadeComponent = ({ atividade }) => {
-    console.log("TCL: AtividadeComponent -> atividade", atividade)
     return (
         <div className="atividade">
           <div className="box">
             <h4>{atividade.titulo}</h4>
             <label>{`das ${atividade.inicio.slice(11, -4)} às ${atividade.fim.slice(11, -4)}`}</label>
-            <h5>{atividade.descricao}</h5>
           </div>
         </div>
     );
