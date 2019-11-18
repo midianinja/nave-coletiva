bd20b147ff1312e29c8ebfbe0366827cc727e8fa
Luis Fagundes <lhfagundes@gmail.com>
2019-11-17 21:45:53 -0300

    cache

--- backend/utils/viewsets.py
+++ backend/utils/viewsets.py
@@ -1,6 +1,18 @@
 from rest_framework import viewsets, mixins
+from django.views.decorators.cache import cache_page
+from django.utils.decorators import method_decorator
+from django.db.models.signals import post_save
+from django.core.cache import cache
 
 class ModelViewSet(mixins.RetrieveModelMixin,
                    mixins.ListModelMixin,
                    viewsets.GenericViewSet):
-    pass
+
+    @method_decorator(cache_page(60*10))
+    def get(self, request, format=None):
+        super().get(request, format)
+
+
+def clear_cache(*args, **kwargs):
+    cache.clear()
+post_save.connect(clear_cache)
