d65073a73b80728118681f37398a359354591cbd
Luis Fagundes <lhfagundes@gmail.com>
2019-11-18 20:37:43 -0300

    filtro por data

--- backend/festival/admin.py
+++ backend/festival/admin.py
@@ -1,9 +1,40 @@
+from datetime import datetime
 from django.contrib import admin
+from django.db.models import Q
 from mptt.admin import DraggableMPTTAdmin
 from admin_auto_filters.filters import AutocompleteFilter
 
 from festival.models import Festival, Encontro, Categoria, Atividade, Tag
 from festival.forms import AtividadeAdminForm
+
+class DateFilter(admin.SimpleListFilter):
+
+    title = 'Data'
+    parameter_name = 'inicio'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('21/11', '21/11'),
+            ('22/11', '22/11'),
+            ('23/11', '23/11'),
+            ('24/11', '24/11'),
+        )
+
+    def queryset(self, request, queryset):
+        if self.value() is None:
+            return queryset
+        day = int(self.value()[:2])
+        inicio = datetime(2019, 11, day, 10, 0)
+        fim = datetime(2019, 11, day, 23, 59)
+        time_filters = (Q(inicio__lte=inicio,
+                          fim__gt=inicio) |
+                        Q(inicio__lt=fim,
+                          fim__gte=fim) |
+                        Q(inicio__lte=inicio,
+                          fim__gte=fim) |
+                        Q(inicio__gte=inicio,
+                          fim__lte=fim))
+        return queryset.filter(time_filters)
 
 class EncontroFilter(AutocompleteFilter):
     title = 'Encontro'
@@ -35,7 +66,7 @@ class EspacoFilter(AutocompleteFilter):
 
 class AtividadeAdmin(admin.ModelAdmin):
     form = AtividadeAdminForm
-    list_filter = ['pendente', EspacoFilter, EncontroFilter, CategoriasFilter, TagsFilter, RedeFilter, ResponsavelFilter, ConvidadeFilter]
+    list_filter = [DateFilter, 'pendente', EspacoFilter, EncontroFilter, CategoriasFilter, TagsFilter, RedeFilter, ResponsavelFilter, ConvidadeFilter]
     list_display = ['titulo', 'espaco', 'inicio_fmt', 'fim_fmt', 'coluna', 'convidades_list']
     search_fields = ['titulo', 'descricao']
     autocomplete_fields = ['categorias', 'tags', 'responsavel', 'convidades']
